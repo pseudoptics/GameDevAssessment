9980320f3fa16bca26945604fc520b1016e47066
pseudoptics <cameronwang2000@gmail.com>
2021-09-27 01:59:55 +1000

    added triggers to control animation states in PacMovement.cs script

--- Assets/Scripts/PacMovement.cs
+++ Assets/Scripts/PacMovement.cs
@@ -6,6 +6,8 @@ public class PacMovement : MonoBehaviour
 {
     [SerializeField]
     private GameObject pacStudent;
+    [SerializeField]
+    private Animator animatorController;
     private Tweener tweener;
 
     // Start is called before the first frame update
@@ -18,17 +20,25 @@ public class PacMovement : MonoBehaviour
     void Update()
     {
         if (Vector3.Equals(pacStudent.transform.position, new Vector3(-12.47f, 12.97f, 0f))) {
+            animatorController.ResetTrigger("UpTrigger");
+            animatorController.SetTrigger("RightTrigger");
             tweener.CreateTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-7.46f, 12.97f, 0f), 3f);
         }
         else if (Vector3.Equals(pacStudent.transform.position, new Vector3(-7.46f, 12.97f, 0f))) {
+            animatorController.ResetTrigger("RightTrigger");
+            animatorController.SetTrigger("DownTrigger");
             tweener.CreateTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-7.46f, 9.02f, 0f), 3f);
         }
         else if (Vector3.Equals(pacStudent.transform.position, new Vector3(-7.46f, 9.02f, 0f)))
         {
+            animatorController.ResetTrigger("DownTrigger");
+            animatorController.SetTrigger("LeftTrigger");
             tweener.CreateTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-12.47f, 9.02f, 0f), 3f);
         }
         else if (Vector3.Equals(pacStudent.transform.position, new Vector3(-12.47f, 9.02f, 0f)))
         {
+            animatorController.ResetTrigger("LeftTrigger");
+            animatorController.SetTrigger("UpTrigger");
             tweener.CreateTween(pacStudent.transform, pacStudent.transform.position, new Vector3(-12.47f, 12.97f, 0f), 3f);
         }
     }
