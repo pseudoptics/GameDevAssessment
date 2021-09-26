263ba6b413e0831663ef1c438dc07dd083013faf
pseudoptics <cameronwang2000@gmail.com>
2021-09-26 16:01:12 +1000

    added transitions between pac-student movements and death

--- Assets/Scripts/PacAnimation.cs
+++ Assets/Scripts/PacAnimation.cs
@@ -6,47 +6,30 @@ public class PacAnimation : MonoBehaviour
 {
     [SerializeField]
     private Animator animatorController;
-    private SpriteRenderer pacSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        pacSprite = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("w")) {
-            if (animatorController.GetCurrentAnimatorStateInfo(0).IsName("PacHorizontalWalkAnim")) {
-                animatorController.SetTrigger("VerticalTrigger");
-            }
-            pacSprite.flipX = false;
-            pacSprite.flipY = false;
+            animatorController.SetTrigger("UpTrigger");
         }
 
         if (Input.GetKeyDown("a")) {
-            if (animatorController.GetCurrentAnimatorStateInfo(0).IsName("PacVerticalWalkAnim")) {
-                animatorController.SetTrigger("HorizontalTrigger");
-            }
-            pacSprite.flipX = true;
-            pacSprite.flipY = false;
+            animatorController.SetTrigger("LeftTrigger");
         }
 
         if (Input.GetKeyDown("s")) {
-            if (animatorController.GetCurrentAnimatorStateInfo(0).IsName("PacHorizontalWalkAnim")) {
-                animatorController.SetTrigger("VerticalTrigger");
-            }
-            pacSprite.flipX = false;
-            pacSprite.flipY = true;
+            animatorController.SetTrigger("DownTrigger");
         }
 
         if (Input.GetKeyDown("d")) {
-            if (animatorController.GetCurrentAnimatorStateInfo(0).IsName("PacVerticalWalkAnim")) {
-                animatorController.SetTrigger("HorizontalTrigger");
-            }
-            pacSprite.flipX = false;
-            pacSprite.flipY = false;
+            animatorController.SetTrigger("RightTrigger");
         }
     }
 }
