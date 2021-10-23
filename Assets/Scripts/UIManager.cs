5153434d1e1891b6ed4e8ed8dd6a043b8c125b40
pseudoptics <cameronwang2000@gmail.com>
2021-10-23 13:22:18 +1100

    added level 1 and exit button functionality

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private Button button;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            button = GameObject.FindWithTag("ExitButton").GetComponent<Button>();
+            button.onClick.AddListener(ExitGame);
+        }
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.LoadScene(0);
+    }
+}
