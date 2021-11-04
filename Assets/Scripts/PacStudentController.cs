e32108862d584d6eb039b942075a5a3bd6675f15
pseudoptics <cameronwang2000@gmail.com>
2021-11-04 20:45:08 +1100

    added pac student wall collision

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -18,22 +18,34 @@ public class PacStudentController : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown("w")) {
-            tweener.CreateTween(transform, transform.position, (transform.position + new Vector3(0, 1, 0)), 0.3f);
+            if (!Physics2D.Raycast(transform.position, new Vector2(0, 1), 1f)) {
+                tweener.CreateTween(transform, transform.position, (transform.position + new Vector3(0, 1, 0)), 0.3f);
+            }
+            else
+            {
+                Debug.Log(Physics2D.Raycast(transform.position, new Vector2(0, 1), 1f));
+            }
             lastInput = "w";
         }
 
         if (Input.GetKeyDown("a")) {
-            tweener.CreateTween(transform, transform.position, (transform.position - new Vector3(1, 0, 0)), 0.3f);
+            if (!Physics2D.Raycast(transform.position, new Vector2(-1, 0), 1f)) {
+                tweener.CreateTween(transform, transform.position, (transform.position - new Vector3(1, 0, 0)), 0.3f);
+            }
             lastInput = "a";
         }
 
         if (Input.GetKeyDown("s")) {
-            tweener.CreateTween(transform, transform.position, (transform.position - new Vector3(0, 1, 0)), 0.3f);
+            if (!Physics2D.Raycast(transform.position, new Vector2(0, -1), 1f)) {
+                tweener.CreateTween(transform, transform.position, (transform.position - new Vector3(0, 1, 0)), 0.3f);
+            }
             lastInput = "s";
         }
 
         if (Input.GetKeyDown("d")) {
-            tweener.CreateTween(transform, transform.position, (transform.position + new Vector3(1, 0, 0)), 0.3f);
+            if (!Physics2D.Raycast(transform.position, new Vector2(1, 0), 1f)) {
+                tweener.CreateTween(transform, transform.position, (transform.position + new Vector3(1, 0, 0)), 0.3f);
+            }
             lastInput = "d";
         }
     }
